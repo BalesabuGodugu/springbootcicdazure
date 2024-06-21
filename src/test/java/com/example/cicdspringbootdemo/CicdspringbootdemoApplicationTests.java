47aec4047d0de8eeb96ab3790ae8094e1323a527
balesabu <balesabu@jss-iMac.local>
2024-06-21 15:27:31 +0530

    initial commit

---
+++ src/test/java/com/example/cicdspringbootdemo/CicdspringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdspringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
